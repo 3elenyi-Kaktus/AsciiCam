9049365af2157dfb3238160a73635a6a5e8575cf
Fedor Fatkullin <myhare.ff@gmail.com>
2023-03-29 17:51:39 +0300

    Optimize ascii converter
    (Not working without further changes to webcam frames preprocessing).
    Ascii converter changrd to work with bijectional frames,
    without pixel chunks reading and interpreting.

--- code/ascii_converter.cpp
+++ code/ascii_converter.cpp
@@ -1,19 +1,7 @@
 #include "ascii_converter.h"
 #include "iostream"
 
-u_char GetPixelsBrightness(cv::Mat &frame, size_t height_pos, size_t width_pos) {
-    double total_brightness = 0;
-    for (size_t i = height_pos; i < height_pos + ASCII_SYMBOL_HEIGHT; ++i) {
-        for (size_t j = width_pos; j < width_pos + ASCII_SYMBOL_WIDTH; ++j) {
-            total_brightness += (uchar)*(frame.ptr(i) + j);
-        }
-    }
-    int average_brightness = round(total_brightness / (ASCII_SYMBOL_HEIGHT * ASCII_SYMBOL_WIDTH));
-    return average_brightness;
-}
 
-
-// Assuming that frame is (height * 15) x (width * 9), with integer constants
 std::vector<std::vector<u_char>> ConvertFrameToASCII(cv::Mat &frame) {
     std::unordered_map<u_char, char> characters = {{0, ' '},
                                                    {1, '.'},
@@ -22,14 +10,14 @@ std::vector<std::vector<u_char>> ConvertFrameToASCII(cv::Mat &frame) {
                                                    {4, '4'},
                                                    {5, 'W'},
                                                    {6, '@'}};
-    size_t out_height = frame.rows / ASCII_SYMBOL_HEIGHT;
-    size_t out_width = frame.cols / ASCII_SYMBOL_WIDTH;
+    size_t out_height = frame.rows;
+    size_t out_width = frame.cols;
     std::vector<std::vector<u_char>> brightness_table(out_height, std::vector<u_char>(out_width));
     u_char min_br = UCHAR_MAX;
     u_char max_br = 0;
     for (size_t i = 0; i < out_height; ++i) {
         for (size_t j = 0; j < out_width; ++j) {
-            brightness_table[i][j] = GetPixelsBrightness(frame, i * ASCII_SYMBOL_HEIGHT, j * ASCII_SYMBOL_WIDTH);
+            brightness_table[i][j] = (uchar) *(frame.ptr(i) + j);
             min_br = std::min(min_br, brightness_table[i][j]);
             max_br = std::max(max_br, brightness_table[i][j]);
         }
--- code/ascii_converter.h
+++ code/ascii_converter.h
@@ -7,11 +7,4 @@
 
 typedef unsigned char u_char;
 
-enum {
-    ASCII_SYMBOL_HEIGHT = 18,
-    ASCII_SYMBOL_WIDTH = 9
-};
-
-u_char GetPixelsBrightness(cv::Mat &frame, size_t height_pos, size_t width_pos);
-
 std::vector<std::vector<u_char>> ConvertFrameToASCII(cv::Mat &frame);
